cb7f2d79b7049de91a482148b78b9279542e9635
Mubeen-creator <142151394+Mubeen-creator@users.noreply.github.com>
2024-04-22 15:32:23 -0700

    feat: student app concept added

--- todos/main.py
+++ todos/main.py
@@ -4,24 +4,49 @@ import uvicorn
 
 app = FastAPI()
 
+students = [
+    {
+        "userName": "Mubeen",
+        "rollNo": 2342
+    },
+    {
+        "userName": "Arsalan",
+        "rollNo": 6467
+    }
+]
+@app.get("/students")
+def getStudents():
+    return students
+
+
+@app.get("/addStudent")
+def addStudent(userName:str, rollNo:str):
+    global studentss
+    students.append({"userName": userName, "rollNo":rollNo})
+    return students
+    
+
+
 @app.get("/")
 def helloWorld():
     return "Hello World"
 
-@app.get("/gettodos")
-def getTodos():
-    print("Get todos called")
-    return "gettodos called"
+# path variables
+@app.get("/gettodos/{id}")
+def getTodos(id):
+    print("Get todos called", id)
+    return id
 
 @app.post("/gettodos")
 def getTodos():
     print("Get post method todos called")
     return "post gettodos called"
 
+# query parms
 @app.get("/getSingleTodo")
-def getSingleTodo():
-    print("Get getSingleTodo called")
-    return "getSingleTodo called sdf"
+def getSingleTodo(userName:str, rollNo:str):
+    print("Get getSingleTodo called", userName, rollNo)
+    return "Get getSingleTodo"
 
 @app.put("/updateTodo")
 def updateTodo():
